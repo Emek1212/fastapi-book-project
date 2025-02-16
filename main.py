0f1d253d837cf476b4c0cb7d9607a66e5f038610
Ubuntu <ubuntu@ip-172-31-25-170.eu-north-1.compute.internal>
2025-02-16 03:45:04 +0000

    Added /stage2 route

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from api.router import api_router
-
-from core.config import settings
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.get("/")
+def home():
+    return {"message": "Welcome to Stage 1"}
 
-app.include_router(api_router, prefix=settings.API_PREFIX)
+@app.get("/stage2")
+def stage2():
+    return {"message": "Welcome to Stage 2"}
 
-
-@app.get("/healthcheck")
-async def health_check():
-    """Checks if server is active."""
-    return {"status": "active"}
