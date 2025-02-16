0108ffadba1dd913dbe28bc13560d610de4293a4
Emek1212 <abasiemekakpan7@gmail.com>
2025-02-15 22:50:28 -0600

    Update config.py

--- core/config.py
+++ core/config.py
@@ -7,9 +7,12 @@ class Settings(BaseSettings):
     PROJECT_VERSION: str = "0.0.1"
     PROJECT_DESCRIPTION: str = "HNG12 DEVOPS x BACKEND (Stage 2)"
     API_PREFIX: str = "/api/v1"
-    SECRET_KEY: str = secrets.token_urlsafe(32)
+    SECRET_KEY: str = secrets.token_urlsafe(32)  # Can be set in an env file instead of generating
     DEBUG: bool = False
     TESTING: bool = False
+
+    class Config:
+        env_file = ".env"  # Use this if you want to load variables from a .env file
 
 
 settings = Settings()
